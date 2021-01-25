03c7f93a53caa4c37fd67edad02b6ef299fc8fdd
jibinkoshy <jibinkoshy@hotmail.com>
2021-01-24 22:35:49 -0600

    Call fakestore api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+//Components
+import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+//styles
+import { Wrapper } from './App.styles';
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 function App() {
-  return (
-    <div className="App">
-    Hello
-    </div>
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
   );
+  console.log('data---', data);
+
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
